21c56a9d3d0d9f2f8a5f899fc6cf838a1c5bfd24
Jake Adams <jdadams@utah.gov>
2020-01-31 11:18:04 -0700

    documenting checks and fixes

--- fixes.py
+++ fixes.py
@@ -1,6 +1,14 @@
 import arcgis
 
 def tags_or_title_fix(item, title=None, tags=None):
+    '''
+    Use item.update() to update title and/or tags.
+    title:  New title as a string
+    tags:   New tags as a list of strings
+
+    return: Update results as a string
+    '''
+
     #: Tags and title combined .update()
     update_dict = {}
     messages = []
@@ -20,6 +28,12 @@ def tags_or_title_fix(item, title=None, tags=None):
 
 
 def group_fix(item, group):
+    '''
+    Use item.share() to share to group
+
+    return: Sharing results as a string
+    '''
+
     #: Group
     share_results = item.share(everyone=True, groups=[group])
     success = share_results['results'][0]['success']
@@ -32,6 +46,12 @@ def group_fix(item, group):
 
 
 def folder_fix(item, folder):
+    '''
+    Use item.move() to move item to folder.
+
+    return: Move results as a string
+    '''
+
     #: Folder
     move_result = item.move(folder)
     if move_result['success']:
@@ -43,6 +63,12 @@ def folder_fix(item, folder):
 
 
 def delete_protection_fix(item):
+    '''
+    Use item.protect() to prevent item from being deleted.
+
+    return: Protection result as a string
+    '''
+
     #: Delete Protection
     protect_result = item.protect(True)
     if protect_result['success']:
@@ -54,6 +80,12 @@ def delete_protection_fix(item):
 
 
 def downloads_fix(item):
+    '''
+    Create a FeatureLayerCollection from item and use it's manager object to allow downloads by adding 'Extract' to it's capabilities.
+
+    return: Download enabling results as a string
+    '''
+
     #: Enable Downloads
     manager = arcgis.features.FeatureLayerCollection.fromitem(item).manager
     download_result = manager.update_definition({ 'capabilities': 'Query,Extract' })
