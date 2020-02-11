c3e5032d94b0891917d6f038202a29061ab8b1ca
Jake Adams <jdadams@utah.gov>
2020-02-11 15:52:09 -0700

    title with agol metatable

---
+++ __main__.py
@@ -0,0 +1,23 @@
+'''
+validate.py
+
+Usage:
+    validate.py validate --org=<org> --user=<user> [--save_report=<report_path>]
+
+Arguments:
+    org           AGOL Portal to connect to [default: https://www.arcgis.com]
+    user          AGOL User for authentication
+    report_path   Folder to save report to, eg `c:\\temp`
+
+Examples:
+    validate.py validate --org=https://www.arcgis.com --user=me --save_report=c:\\temp
+'''
+
+from docopt import docopt
+
+from validate import validator
+
+
+def main():
+
+    args = docopt(__doc__, version = '0.8.0')
