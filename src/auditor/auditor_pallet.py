8eaf682f6b214fa35918e4b032f348dde3c927e9
Jake Adams <jdadams@utah.gov>
2020-07-29 17:38:45 -0600

    pylint satisfaction

--- src/auditor/auditor_pallet.py
+++ src/auditor/auditor_pallet.py
@@ -1,13 +1,18 @@
+"""
+auditor_pallet.py: forklift-compliant entry point
+"""
 from forklift.models import Pallet
 
 from . import auditor
 
+
 class AuditorPallet(Pallet):
+
     def requires_processing(self):
         #: No crates, run process every time
         return True
 
-    def process():
+    def process(self):
         org_auditor = auditor.Auditor()
         org_auditor.check_items()
         org_auditor.fix_items()
