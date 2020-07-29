f818d46dbee73e2f1a4f8e51b9a0a668c1fc365d
Jake Adams <jdadams@utah.gov>
2020-07-29 08:31:29 -0600

    feat: moving to src

--- credentials_template.py
+++
@@ -1,8 +0,0 @@
-ORG =  #: URL to AGOL organization (https://www.arcgis.com)
-USERNAME =  #: User whose items will be validated
-PASSWORD =  #: USERNAME's password
-DB =  #: Full path to sde connection file
-THUMBNAIL_DIR =  #: Directory with thumbnails named sgid_group.png
-METATABLE =  #: Full path to SGID.META.AGOLItems metatable
-AGOL_TABLE =  #: URL for Feature Service REST endpoint for AGOL-hosted metatable
-XML_TEMPLATE =  #: Path to 'exact copy of.xslt' template
