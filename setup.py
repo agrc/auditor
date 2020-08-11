04e77ebd0512c0888779b7fff6f4fe7993caa882
Jake Adams <jdadams@utah.gov>
2020-08-11 14:28:14 -0600

    feat: bump version for logging

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='auditor',
-    version='1.0.0',
+    version='1.1.0',
     license='MIT',
     description=(
         'Audits all hosted feature service items in a user\'s AGOL folders for proper tags, sharing, etc based on '
