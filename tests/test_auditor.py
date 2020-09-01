2146feef88d7edfc29a724e719b6f0477860c5d8
Jake Adams <jdadams@utah.gov>
2020-09-01 11:41:10 -0600

    feat: metatable tests

--- tests/test_auditor.py
+++ tests/test_auditor.py
@@ -1,5 +1,4 @@
 import pytest
-from unittest.mock import patch, MagicMock
 
 from auditor.auditor import (retry, Metatable)
 
@@ -18,28 +17,132 @@ def test_retry():
         retry(inner_retry)
 
 
-@patch(arcpy.da.SearchCursor)
-def test_read_sgid_metatable():
+def test_read_sgid_metatable(mocker):
 
-    #: table_sgid_name, table_agol_itemid, table_agol_name, table_authoritative
-    sgid_row = ['SGID.GEOSCIENCE.Minerals', '9d2e949a9492425dbb4e5d5212f9ef19', 'Utah Minerals', None]
-    #: table_sgid_name, table_agol_itemid, table_agol_name, table_category
-    agol_row = [
-        'SGID.WATER.ParagonahStructures', 'dd7fa2d78d2547759a50d6f827f8df3a', 'Utah Paragonah Structures', 'shelved'
+    def return_sgid_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_authoritative
+        for row in [['SGID.GEOSCIENCE.Minerals', '9d2e949a9492425dbb4e5d5212f9ef19', 'Utah Minerals', None]]:
+            yield row
+
+    sgid_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'Authoritative']
+
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_sgid_row)
+
+    test_table = Metatable()
+    test_table.read_metatable('something', sgid_fields)
+
+    # '9d2e949a9492425dbb4e5d5212f9ef19': ['SGID.GEOSCIENCE.Minerals', 'Utah Minerals', 'SGID', None],
+    assert test_table.metatable_dict['9d2e949a9492425dbb4e5d5212f9ef19'] == [
+        'SGID.GEOSCIENCE.Minerals', 'Utah Minerals', 'SGID', None
     ]
+
+
+def test_read_agol_metatable(mocker):
+
+    def return_agol_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_category
+        for row in [[
+            'SGID.WATER.ParagonahStructures', 'dd7fa2d78d2547759a50d6f827f8df3a', 'Utah Paragonah Structures', 'shelved'
+        ]]:
+            yield row
+
+    agol_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'CATEGORY']
+
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_agol_row)
+
+    test_table = Metatable()
+    test_table.read_metatable('something', agol_fields)
+
+    assert test_table.metatable_dict['dd7fa2d78d2547759a50d6f827f8df3a'] == [
+        'SGID.WATER.ParagonahStructures', 'Utah Paragonah Structures', 'shelved', 'n'
+    ]
+
+
+def test_magic_word_in_itemid_field(mocker):
+
+    def return_sgid_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_authoritative
+        for row in [['SGID.GEOSCIENCE.Minerals', 'magic_word', 'Utah Minerals', None]]:
+            yield row
+
+    sgid_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'Authoritative']
+
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_sgid_row)
+
+    test_table = Metatable()
+    test_table.read_metatable('something', sgid_fields)
+
+    #: metatable dict should be empty
+    assert test_table.metatable_dict == {}
+
+
+def test_blank_itemid(mocker):
+
+    def return_sgid_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_authoritative
+        for row in [['SGID.GEOSCIENCE.Minerals', '', 'Utah Minerals', None]]:
+            yield row
+
+    sgid_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'Authoritative']
+
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_sgid_row)
+
+    test_table = Metatable()
+    test_table.read_metatable('something', sgid_fields)
+
+    #: metatable dict should be empty
+    assert test_table.metatable_dict == {}
+
+
+def test_duplicate_itemids(mocker):
+
+    def return_sgid_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_authoritative
+        for row in [['SGID.GEOSCIENCE.Minerals', '9d2e949a9492425dbb4e5d5212f9ef19', 'Utah Minerals', None],
+                    ['SGID.GEOSCIENCE.MineralsCopy', '9d2e949a9492425dbb4e5d5212f9ef19', 'Utah Minerals Copy', None]]:
+            yield row
+
+    sgid_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'Authoritative']
+
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_sgid_row)
+
+    test_table = Metatable()
+    test_table.read_metatable('something', sgid_fields)
+
+    #: Our duplicate id should be the only entry in .duplicate_keys and .metatable_dict should just have first item
+    assert test_table.duplicate_keys == ['9d2e949a9492425dbb4e5d5212f9ef19']
+    assert test_table.metatable_dict['9d2e949a9492425dbb4e5d5212f9ef19'] == [
+        'SGID.GEOSCIENCE.Minerals', 'Utah Minerals', 'SGID', None
+    ]
+
+
+def test_duplicate_itemids_from_different_tables(mocker):
+
+    def return_sgid_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_authoritative
+        for row in [['SGID.GEOSCIENCE.Minerals', '9d2e949a9492425dbb4e5d5212f9ef19', 'Utah Minerals', None]]:
+            yield row
+
+    def return_agol_row(self, table, fields):
+        #: table_sgid_name, table_agol_itemid, table_agol_name, table_category
+        for row in [[
+            'SGID.WATER.ParagonahStructures', '9d2e949a9492425dbb4e5d5212f9ef19', 'Utah Paragonah Structures', 'shelved'
+        ]]:
+            yield row
+
     sgid_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'Authoritative']
     agol_fields = ['TABLENAME', 'AGOL_ITEM_ID', 'AGOL_PUBLISHED_NAME', 'CATEGORY']
 
-    # cursor_mock = unittest.MagicMock()
-    # cursor_mock.__iter__.return_value = [sgid_row, agol_row]
-
-    arcpy.da.SearchCursor = MagicMock()
-    arcpy.da.SearchCursor.__inter__.return_value = [sgid_row]
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_sgid_row)
 
     test_table = Metatable()
-    test_table.read_metatable(None, [sgid_fields])
+    test_table.read_metatable('something', sgid_fields)
 
-    # '9d2e949a9492425dbb4e5d5212f9ef19': ['SGID.GEOSCIENCE.Minerals', 'Utah Minerals', 'SGID', None],
+    mocker.patch('auditor.auditor.Metatable._cursor_wrapper', return_agol_row)
+    test_table.read_metatable('agol something', agol_fields)
+
+    #: Our duplicate id should be the only entry in .duplicate_keys and .metatable_dict should just have first item
+    assert test_table.duplicate_keys == ['9d2e949a9492425dbb4e5d5212f9ef19']
     assert test_table.metatable_dict['9d2e949a9492425dbb4e5d5212f9ef19'] == [
         'SGID.GEOSCIENCE.Minerals', 'Utah Minerals', 'SGID', None
     ]
