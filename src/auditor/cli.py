6b5666956f6532659db2931c77937cabcf1ace1b
Jake Adams <jdadams@utah.gov>
2020-12-30 10:09:44 -0700

    feat: separate commands for spot, scheduled

--- src/auditor/cli.py
+++ src/auditor/cli.py
@@ -2,7 +2,8 @@
 auditor
 
 Usage:
-    auditor [--save_report --dry --verbose ITEM ...]
+    auditor spot [--save_report --dry --verbose ITEM ...] 
+    auditor scheduled
 
 Options:
     -h, --help
@@ -45,55 +46,68 @@ def cli():
         print(__doc__)
     else:
 
-        # report_dir = args['--save_report']
-
         summary_logger = logging.getLogger(__name__)
         summary_logger.setLevel(logging.DEBUG)
-        detailed_formatter = logging.Formatter(
-            fmt='%(levelname)-7s %(asctime)s %(module)10s:%(lineno)5s %(message)s', datefmt='%m-%d %H:%M:%S'
-        )
 
-        #: Only dump summary to the console if verbose
-        if args['--verbose']:
-            cli_handler = logging.StreamHandler(stream=sys.stdout)
-            cli_handler.setLevel(logging.DEBUG)
-            cli_handler.setFormatter(detailed_formatter)
-            summary_logger.addHandler(cli_handler)
+        if args['spot']:
+            #: Only dump summary to the console if verbose
+            if args['--verbose']:
+                cli_handler = logging.StreamHandler(stream=sys.stdout)
+                cli_handler.setLevel(logging.DEBUG)
+                cli_formatter = logging.Formatter(
+                    fmt='%(levelname)-7s %(asctime)s %(module)10s:%(lineno)5s %(message)s', datefmt='%m-%d %H:%M:%S'
+                )
+                cli_handler.setFormatter(cli_formatter)
+                summary_logger.addHandler(cli_handler)
 
-        #: Create a string stream for summary report
-        summary_stream = StringIO()
-        summary_handler = logging.StreamHandler(stream=summary_stream)
-        summary_handler.setFormatter(detailed_formatter)
-        summary_logger.addHandler(summary_handler)
+            org_auditor = Auditor(summary_logger, args['--verbose'], args['ITEM'])
 
-        #: set up supervisor, add email handler
-        auditor_supervisor = Supervisor(
-            project_name='auditor', logger=summary_logger, log_path=credentials.REPORT_BASE_PATH
-        )
-        email_settings = {
-            'smtpServer': 'send.state.ut.us',
-            'smtpPort': 25,
-            'from_address': 'noreply@utah.gov',
-            'to_addresses': 'jdadams@utah.gov',
-            'prefix': f'Auditor on {socket.gethostname()}: ',
-        }
-        auditor_supervisor.add_message_handler(EmailHandler(email_settings))
+            if args['--dry']:
+                org_auditor.check_items(args['--save_report'])
+            else:
+                org_auditor.check_items(report=False)  #: only do the fix report on a full run.
+                org_auditor.fix_items(args['--save_report'])
 
-        org_auditor = Auditor(summary_logger, args['--verbose'], args['ITEM'])
+            org_auditor.check_organization_wide()
 
-        if args['--dry']:
-            org_auditor.check_items(args['--save_report'])
+        elif args['scheduled']:
+            #: Create a string stream for summary report
+            summary_stream = StringIO()
+            summary_handler = logging.StreamHandler(stream=summary_stream)
+            stream_formatter = logging.Formatter(
+                fmt='%(levelname)-7s %(asctime)s %(module)10s:%(lineno)5s %(message)s<br />', datefmt='%m-%d %H:%M:%S'
+            )
+            summary_handler.setFormatter(stream_formatter)
+            summary_logger.addHandler(summary_handler)
+
+            #: set up supervisor, add email handler
+            auditor_supervisor = Supervisor(
+                project_name='auditor', logger=summary_logger, log_path=credentials.REPORT_BASE_PATH
+            )
+            email_settings = {
+                'smtpServer': 'send.state.ut.us',
+                'smtpPort': 25,
+                'from_address': 'noreply@utah.gov',
+                'to_addresses': 'jdadams@utah.gov',
+                'prefix': f'Auditor on {socket.gethostname()}: ',
+            }
+            auditor_supervisor.add_message_handler(EmailHandler(email_settings))
+
+            org_auditor = Auditor(summary_logger)
+            org_auditor.check_organization_wide()
+            org_auditor.check_items(report=False)  #: Don't bother reporting checks
+            org_auditor.fix_items(report=True)
+
+            #: Build and send summary message
+            summary_message = MessageDetails()
+            summary_message.message = summary_stream.getvalue()
+            summary_message.project_name = 'auditor'
+            summary_message.attachments = [credentials.REPORT_BASE_PATH]
+            summary_message.subject = f'Auditor Report {datetime.datetime.today()}'
+
+            auditor_supervisor.notify(summary_message)
+
         else:
-            org_auditor.check_items(report=False)  #: only do the fix report on a full run.
-            org_auditor.fix_items(args['--save_report'])
-
-        org_auditor.check_organization_wide()
-
-        #: Build and send summary message
-        summary_message = MessageDetails()
-        summary_message.message = summary_stream.getvalue()
-        summary_message.project_name = 'auditor'
-        summary_message.attachments = [credentials.REPORT_BASE_PATH]
-        summary_message.subject = f'Auditor Report {datetime.datetime.today()}'
-
-        auditor_supervisor.notify(summary_message)
+            raise NotImplementedError(
+                'Command not recognized. Please run either "spot" or "scheduled" audit (see help)'
+            )
