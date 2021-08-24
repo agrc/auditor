581884e7f9b7edeb9b7bfd360ec6e9209d2c7b2b
Jake Adams <jdadams@utah.gov>
2021-08-24 13:05:29 -0600

    chore: minor version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='auditor',
-    version='2.2.1',
+    version='2.3.0',
     license='MIT',
     description=(
         'Audits all hosted feature service items in a user\'s AGOL folders for proper tags, sharing, etc based on '
