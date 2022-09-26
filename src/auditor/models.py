020ff9f2f187a64a695d62c428955d1f9598a5a3
Jake Adams <jdadams@utah.gov>
2022-09-26 10:20:13 -0600

    chore: DABS should be uppercase

--- src/auditor/models.py
+++ src/auditor/models.py
@@ -171,12 +171,12 @@ class Auditor:
 
     #: Tags or words that should be uppercased, saved as lower to check against
     uppercased_tags = [
-        '2g', '3g', '4g', 'agol', 'aog', 'at&t', 'atv', 'blm', 'brat', 'caf', 'cdl', 'dabc', 'daq', 'dem', 'dfcm',
-        'dfirm', 'dnr', 'dogm', 'dot', 'dsl', 'dsm', 'dtm', 'dup', 'dwq', 'e911', 'ems', 'epa', 'fae', 'fcc', 'fema',
-        'gcdb', 'gis', 'gnis', 'hava', 'huc', 'lir', 'lrs', 'lte', 'luca', 'mrrc', 'nca', 'ng911', 'ngda', 'nox',
-        'npsbn', 'ntia', 'nwi', 'osa', 'pli', 'plss', 'pm10', 'ppm', 'psap', 'sao', 'sbdc', 'sbi', 'sgid', 'shpo',
-        'sitla', 'sligp', 'trax', 'uca', 'udot', 'ugrc', 'ugs', 'uhp', 'uic', 'uipa', 'us', 'usao', 'usdw', 'usfs',
-        'usfws', 'usps', 'ustc', 'ut', 'uta', 'utsc', 'vcp', 'vista', 'voc', 'wbd', 'wre'
+        '2g', '3g', '4g', 'agol', 'aog', 'at&t', 'atv', 'blm', 'brat', 'caf', 'cdl', 'dabc', 'dabs', 'daq', 'dem',
+        'dfcm', 'dfirm', 'dnr', 'dogm', 'dot', 'dsl', 'dsm', 'dtm', 'dup', 'dwq', 'e911', 'ems', 'epa', 'fae', 'fcc',
+        'fema', 'gcdb', 'gis', 'gnis', 'hava', 'huc', 'lir', 'lrs', 'lte', 'luca', 'mrrc', 'nca', 'ng911', 'ngda',
+        'nox', 'npsbn', 'ntia', 'nwi', 'osa', 'pli', 'plss', 'pm10', 'ppm', 'psap', 'sao', 'sbdc', 'sbi', 'sgid',
+        'shpo', 'sitla', 'sligp', 'trax', 'uca', 'udot', 'ugrc', 'ugs', 'uhp', 'uic', 'uipa', 'us', 'usao', 'usdw',
+        'usfs', 'usfws', 'usps', 'ustc', 'ut', 'uta', 'utsc', 'vcp', 'vista', 'voc', 'wbd', 'wre'
     ]
 
     #: Articles that should be left lowercase.
